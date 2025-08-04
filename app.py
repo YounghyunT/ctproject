4df939384ed35f3dbf286d704ae4298f634328bb
YounghyunT <teacher.younghyun@gmail.com>
2025-08-04 17:21:14 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.figure_factory as ff
-import plotly.express as px
-import matplotlib.pyplot as pit
+
 
 st.set_page_config(layout='wide', page_title='My age')
 
